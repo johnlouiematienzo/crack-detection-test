d48b389b48f47854c37bf56007d692eab539a2eb
John Louie Matienzo <johnlouie.matienzo@volenday.com>
2024-12-05 11:06:18 +0800

    implemented image classifier with ai trained on custom image using mobilenet feature model

--- src/app/components/MainContent.jsx
+++ src/app/components/MainContent.jsx
@@ -1,21 +1,21 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
+// import "@tensorflow/tfjs-backend-cpu";
 import "@tensorflow/tfjs-backend-webgl";
-// import Script from "next/script";
 
-import {
-  Box,
-  Button,
-  Chip,
-  CircularProgress,
-  Skeleton,
-  Typography,
-} from "@mui/material";
+import { Box, Button, Chip, CircularProgress, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import { enqueueSnackbar } from "notistack";
 
-import * as mobilenet from "@tensorflow-models/mobilenet";
+// import * as mobilenet from "@tensorflow-models/mobilenet";
+// import * as cocossd from "@tensorflow-models/coco-ssd";
+import * as tf from "@tensorflow/tfjs";
+const MOBILE_NET_INPUT_WIDTH = 224;
+const MOBILE_NET_INPUT_HEIGHT = 224;
+const MOBILE_NET_URL =
+  // "https://www.kaggle.com/models/google/mobilenet-v3/TfJs/small-100-224-feature-vector/1";
+  "https://tfhub.dev/google/tfjs-model/imagenet/mobilenet_v3_small_100_224/feature_vector/5/default/1";
 
 const VisuallyHiddenInput = styled("input")({
   clip: "rect(0 0 0 0)",
@@ -31,7 +31,16 @@ const VisuallyHiddenInput = styled("input")({
 
 const MainContent = () => {
   const [isModelLoading, setIsModelLoading] = useState(false);
-  const [model, setModel] = useState(null);
+  const [isModelTraining, setIsModelTraining] = useState(false);
+  const [model, setModel] = useState(null); //finalModel
+  const [modelBase, setModelBase] = useState(null); //baseModel
+  const [modelTrain, setModelTrain] = useState(null); //trainedModel
+
+  const [inputImageGood, setInputImageGood] = useState([]);
+  const [inputImageBad, setInputImageBad] = useState([]);
+  const [trainingDataInputs, setTrainingDataInputs] = useState([]);
+  const [trainingDataOutputs, setTrainingDataOutputs] = useState([]);
+  const classnames = ["GOOD", "BAD"];
 
   const [srcImage, setSrcImage] = useState(null);
   const [filename, setFilename] = useState(null);
@@ -39,44 +48,219 @@ const MainContent = () => {
 
   const [results, setResults] = useState([]);
 
+  const handleInputImageGoodChange = (event) => {
+    const files = event.target.files;
+    const newImages = [];
+
+    for (let i = 0; i < files.length; i++) {
+      newImages.push(URL.createObjectURL(files[i]));
+    }
+    setInputImageGood(newImages);
+  };
+  const handleInputImageBadChange = (event) => {
+    const files = event.target.files;
+    const newImages = [];
+
+    for (let i = 0; i < files.length; i++) {
+      newImages.push(URL.createObjectURL(files[i]));
+    }
+    setInputImageBad(newImages);
+  };
+
   const handleSrcImageChange = (event) => {
     const file = event.target.files[0];
     setSrcImage(URL.createObjectURL(file));
     setFilename(file.name);
     setResults([]);
-
-    // const reader = new FileReader();
-    // reader.onload = (e) => {
-    //   setSrcImage(e.target.result);
-    // };
-    // reader.readAsDataURL(file);
   };
   const onImgLoad = (event) => {
+    const canvas = document.getElementById("canvasOutput");
+    canvas.width = 500;
+    canvas.height = 500;
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(imageRef.current, 0, 0, 500, 500);
+
     identifyImage();
   };
 
   const identifyImage = async () => {
-    const results = await model.classify(imageRef.current);
-    console.log(results);
+    const results = await predict();
     setResults(results);
   };
 
-  const loadModel = async () => {
+  const loadBaseModel = async () => {
     setIsModelLoading(true);
     try {
-      const model = await mobilenet.load();
-      setModel(model);
+      const mobilenet_model = await tf.loadGraphModel(MOBILE_NET_URL, {
+        fromTFHub: true,
+      });
+      tf.tidy(() => {
+        const zero = tf.zeros([
+          1,
+          MOBILE_NET_INPUT_HEIGHT,
+          MOBILE_NET_INPUT_WIDTH,
+          3,
+        ]);
+        const answer = mobilenet_model.predict(zero);
+      });
+      // mobilenet_model.summary();
+      setModelBase(mobilenet_model);
       setIsModelLoading(false);
-      enqueueSnackbar("Model loaded successfully", { variant: "success" });
+      enqueueSnackbar("Base Model loaded successfully", { variant: "success" });
     } catch (error) {
       console.log(error);
       setIsModelLoading(false);
-      enqueueSnackbar("Error loading model", { variant: "error" });
+      enqueueSnackbar("Error loading base model", { variant: "error" });
+    }
+  };
+  const loadModelTrain = async () => {
+    let model = tf.sequential();
+    model.add(
+      tf.layers.dense({ inputShape: [1024], units: 128, activation: "relu" })
+    );
+    model.add(
+      tf.layers.dense({ units: classnames.length, activation: "softmax" })
+    );
+    model.compile({
+      optimizer: "adam",
+      loss: "binaryCrossentropy",
+      metrics: ["accuracy"],
+    });
+    setModelTrain(model);
+  };
+  const getInputImageFeatures = async () => {
+    const goodImages = await Promise.all(
+      inputImageGood.map(async (image) => {
+        const img = new Image();
+        img.src = image;
+        await img.decode();
+        return tf.tidy(() => {
+          const imgTensor = tf.browser.fromPixels(img);
+          const resized = tf.image.resizeBilinear(
+            imgTensor,
+            [MOBILE_NET_INPUT_HEIGHT, MOBILE_NET_INPUT_WIDTH],
+            true
+          );
+          const normalized = resized.div(255);
+          return modelBase.predict(normalized.expandDims(0));
+        });
+      })
+    );
+    const badImages = await Promise.all(
+      inputImageBad.map(async (image) => {
+        const img = new Image();
+        img.src = image;
+        await img.decode();
+        return tf.tidy(() => {
+          const imgTensor = tf.browser.fromPixels(img);
+          const resized = tf.image.resizeBilinear(
+            imgTensor,
+            [MOBILE_NET_INPUT_HEIGHT, MOBILE_NET_INPUT_WIDTH],
+            true
+          );
+          const normalized = resized.div(255.0);
+          return modelBase.predict(normalized.expandDims(0));
+        });
+      })
+    );
+    setTrainingDataInputs(goodImages.concat(badImages));
+    setTrainingDataOutputs(
+      Array(goodImages.length).fill(0).concat(Array(badImages.length).fill(1))
+    );
+  };
+  const trainModel = async () => {
+    try {
+      setIsModelTraining(true);
+      await getInputImageFeatures();
+
+      tf.util.shuffleCombo(trainingDataInputs, trainingDataOutputs);
+
+      let outputsAsTensor = tf.tensor1d(trainingDataOutputs, "int32");
+      let oneHotOutputs = tf.oneHot(outputsAsTensor, classnames.length);
+      let inputsAsTensor = tf.stack(trainingDataInputs).squeeze();
+
+      let results = await modelTrain.fit(inputsAsTensor, oneHotOutputs, {
+        shuffle: true,
+        batchSize: 5,
+        epochs: 5,
+        callbacks: {
+          onEpochEnd: (epoch, logs) => {
+            console.log("Data for epoch " + epoch, logs);
+          },
+        },
+      });
+
+      outputsAsTensor.dispose();
+      oneHotOutputs.dispose();
+      inputsAsTensor.dispose();
+
+      // let combinedModel = tf.sequential();
+      // combinedModel.add(modelBase);
+      // combinedModel.add(modelTrain);
+      // combinedModel.compile({
+      //   optimizer: "adam",
+      //   loss: "binaryCrossentropy",
+      // });
+      // combinedModel.summary();
+      // await combinedModel.save("downloads://my-model");
+      // setModel(combinedModel);
+      setIsModelTraining(false);
+    } catch (error) {
+      console.log(error);
+      setIsModelTraining(false);
     }
   };
 
+  const preprocess = (imageTensor) => {
+    const widthToHeight = imageTensor.shape[1] / imageTensor.shape[0];
+    let squareCrop;
+    if (widthToHeight > 1) {
+      const heightToWidth = imageTensor.shape[0] / imageTensor.shape[1];
+      const cropTop = (1 - heightToWidth) / 2;
+      const cropBottom = 1 - cropTop;
+      squareCrop = [[cropTop, 0, cropBottom, 1]];
+    } else {
+      const cropLeft = (1 - widthToHeight) / 2;
+      const cropRight = 1 - cropLeft;
+      squareCrop = [[0, cropLeft, 1, cropRight]];
+    }
+    const crop = tf.image.cropAndResize(
+      tf.expandDims(imageTensor),
+      squareCrop,
+      [0],
+      [MOBILE_NET_INPUT_HEIGHT, MOBILE_NET_INPUT_WIDTH]
+    );
+    return crop.div(255);
+  };
+
+  const predict = async () => {
+    let imageTensor = tf.browser.fromPixels(imageRef.current);
+    let imageFeatures = modelBase.predict(preprocess(imageTensor));
+    let prediction = modelTrain.predict(imageFeatures).squeeze();
+    let highestIndex = prediction.argMax().arraySync();
+    let predictionArray = prediction.arraySync();
+
+    return classnames.map((className, index) => ({
+      className,
+      probability: predictionArray[index],
+    }));
+  };
+
+  const reset = () => {
+    console.log(trainingDataOutputs);
+    trainingDataInputs.forEach((input) => input.dispose());
+    trainingDataOutputs.forEach((output) => output.dispose());
+    setInputImageGood([]);
+    setInputImageBad([]);
+    setSrcImage(null);
+    setFilename(null);
+    setResults([]);
+    console.log(tf.memory().numTensors);
+  };
+
   useEffect(() => {
-    loadModel();
+    loadBaseModel();
+    loadModelTrain();
   }, []);
 
   if (isModelLoading) {
@@ -97,6 +281,100 @@ const MainContent = () => {
 
   return (
     <>
+      <Box
+        sx={{
+          width: "full",
+          height: "100vh",
+          display: "flex",
+          gap: 4,
+          p: 4,
+        }}
+      >
+        <Box
+          sx={{
+            flex: 1,
+            p: 2,
+            border: "1px solid black",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Button variant="contained" component="label" role={undefined}>
+            Upload Images for Training Good Fruits
+            <VisuallyHiddenInput
+              type="file"
+              id="logo"
+              name="Logo"
+              accept="image/*"
+              multiple
+              onChange={handleInputImageGoodChange}
+            />
+          </Button>
+          <Box sx={{ overflow: "hidden", display: "flex", flexWrap: "wrap" }}>
+            {inputImageGood.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt={`img-${index}`}
+                style={{ width: "100px", height: "100px" }}
+              />
+            ))}
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            flex: 1,
+            p: 2,
+            border: "1px solid black",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Button variant="contained" component="label" role={undefined}>
+            Upload Images for Training Bad Fruits
+            <VisuallyHiddenInput
+              type="file"
+              id="logo"
+              name="Logo"
+              accept="image/*"
+              multiple
+              onChange={handleInputImageBadChange}
+            />
+          </Button>
+          <Box sx={{ overflow: "hidden", display: "flex", flexWrap: "wrap" }}>
+            {inputImageBad.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt={`img-${index}`}
+                style={{ width: "100px", height: "100px" }}
+              />
+            ))}
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+            gap: 2,
+            px: 2,
+          }}
+        >
+          <Button
+            variant="contained"
+            onClick={trainModel}
+            disabled={isModelTraining || isModelLoading}
+            fullWidth
+          >
+            Start Training
+          </Button>
+          <Button variant="contained" color="error" onClick={reset} fullWidth>
+            Reset
+          </Button>
+        </Box>
+      </Box>
       <Box
         sx={{
           width: "full",
@@ -213,14 +491,6 @@ const MainContent = () => {
           </Box>
         </Box>
       </Box>
-      {/* <Script
-        src="/scripts/opencv/opencv.js"
-        onReady={() => console.log("loaded opencv.js")}
-      />
-      <Script
-        src="/scripts/opencv/init.js"
-        onReady={() => console.log("loaded index.js")}
-      /> */}
     </>
   );
 };
--- src/lib/OpenCV/sample.js
+++
@@ -1,8 +0,0 @@
-import cv from "./opencv";
-
-export const showImage = (imgID, canvasID) => {
-  const imgElement = document.getElementById(imgID);
-  const mat = cv.imread(imgElement);
-  cv.imshow(canvasID, mat);
-  mat.delete();
-};
